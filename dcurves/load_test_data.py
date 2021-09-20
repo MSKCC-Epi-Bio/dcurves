b381d94058dbb7d62f624b2f3f48bcd05bd2db2d
shaunporwal <shaun.porwal@gmail.com>
2021-09-20 11:29:40 -0400

    Add in functions to load test data

---
+++ dcurves/load_test_data.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import pkg_resources
+
+def load_binary_df():
+    """Return a dataframe containing the binary data
+    """
+
+    # This is a stream-like object. If you want the actual info, call
+    # stream.read()
+
+    stream = pkg_resources.resource_stream(__name__, 'data/df_binary.csv')
+    return pd.read_csv(stream, encoding='latin-1')
+
+def load_survival_df():
+    """Return a dataframe containing the survival data
+    """
+
+    # This is a stream-like object. If you want the actual info, call
+    # stream.read()
+
+    stream = pkg_resources.resource_stream(__name__, 'data/df_surv.csv')
+    return pd.read_csv(stream, encoding='latin-1')
+
+def load_case_control_df():
+    """Return a dataframe containing the case-control data
+    """
+
+    # This is a stream-like object. If you want the actual info, call
+    # stream.read()
+
+    stream = pkg_resources.resource_stream(__name__, 'data/df_case_control.csv')
+    return pd.read_csv(stream, encoding='latin-1')
+

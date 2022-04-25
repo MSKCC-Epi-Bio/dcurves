5dbb8912f77e1eb60f7efa85000742a443a54a30
shaunporwal <shaun.porwal@gmail.com>
2022-04-24 20:07:22 -0400

    Make dcurves Functional
    Changed structure, added example file for user

---
+++ scripts/run_examples.sh
@@ -0,0 +1,6 @@
+
+# Bash script used to install dcurves package and then run examples.py
+
+pip -q install  ../
+
+python3 ./examples.py
---
+++ scripts/start_python.sh
@@ -0,0 +1,18 @@
+
+
+# In order to use this to add dcurves to python path, have to be CD's into parent directory
+# of package
+
+export PYTHONPATH=.:$PYTHONPATH
+python3 "$@" #
+
+# /Users/ShaunPorwal/Documents/GitHub/python_packages/dcurves/scripts/start_python.sh -m unittest \
+# path/of/test
+
+# ~/
+
+
+
+
+
+

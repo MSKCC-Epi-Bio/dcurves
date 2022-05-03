5e313999cefd8533361b93b06a1fc0fe7a5b1e77
shaunporwal <shaun.porwal@gmail.com>
2022-05-03 15:25:22 -0400

    Edit validation for predictors list to make sure input is list

--- dcurves/validate.py
+++ dcurves/validate.py
@@ -521,10 +521,16 @@ def _calculate_test_consequences_input_checks(
     _validate_time_to_outcome_col(time_to_outcome_col=time_to_outcome_col)
 
 
-def _validate_predictors(predictors: list):
-    for i in predictors:
-        if not isinstance(i, str):
-            raise TypeError('All elements in predictors list must be of type str')
+def _validate_predictors(predictors: list(str)):
+    
+    if predictors is None:
+        pass
+    elif not isinstance(predictors, list):
+        raise TypeError('Predictors must be of type list')
+    else:
+        for i in predictors:
+            if not isinstance(i, str):
+                raise TypeError('All elements in predictors list must be of type str')
 
 
 def _validate_thresh_vals(thresh_vals: list):

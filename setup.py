38a2a936052bf9a7620725a21fa33f9d6dd4ff39
shaunporwal <shaun.porwal@gmail.com>
2022-10-28 13:58:29 -0400

    Update to correct version number
    Since this is first time this branch is being used for these changes, restart versioning from scratch

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.3'
+VERSION = '0.0.1'
 DESCRIPTION = 'Python package for Andrew Vickers\' Decision Curve Analysis method to evaluate prediction models and diagnostic tests'
 LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
 

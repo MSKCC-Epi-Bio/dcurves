0ceb1ffe878542ebf0a3ec56fc09b03fc673e28b
Porwal <PorwalS@mskcc.org>
2022-12-18 19:15:52 -0500

    Simplifying Changes

--- tests/test_binary_dca.py
+++ tests/test_binary_dca.py
@@ -20,7 +20,7 @@ def test_bctr_1():
         _binary_convert_to_risk(
             model_frame=binary_df,
             outcome='cancer',
-            predictor='marker'  # 'marker'
+            predictors_to_prob=['marker']  # 'marker'
         )
 
     r_bctr_result_1_df = r_bctr_result_1_df.round(12)
@@ -83,54 +83,56 @@ def test_b_dca_result_1():
     #           str(i))
     #     assert r_b_dca_result_df['tp_rate'].values[i] == p_b_dca_result_df['tp_rate'].values[i]
 
-def test_b_dca_prev():
-
-    df_binary = load_binary_df()
-    r_b_dca_result_df = load_r_bin_dca_result_2()
-
-    p_b_dca_result_df = \
-        binary_dca(
-            data=df_binary,
-            outcome='cancer',
-            predictors=['age'],
-            probabilities=[True],
-            prevalence=[0.5]
-        )
-
-    del r_b_dca_result_df['label']
-
-    # Rounding to 13 digits to smooth out minute differences
-    # between R and Python calcs
-    r_b_dca_result_df = r_b_dca_result_df.round(13)
-    p_b_dca_result_df = p_b_dca_result_df.round(13)
-
-    p_b_dca_result_df = p_b_dca_result_df.rename(columns={'fpr': 'fp_rate',
-                                                          'tpr': 'tp_rate'})
-
-    r_b_dca_result_df = r_b_dca_result_df.sort_values(by=['variable', 'threshold'])
-    p_b_dca_result_df = p_b_dca_result_df.sort_values(by=['variable', 'threshold'])
-
-    p_b_dca_result_df = p_b_dca_result_df[r_b_dca_result_df.columns]
-
-    # Compare values between r & p binary DCA result
-
-    # test_pos_rate
-    for i in range(0, len(r_b_dca_result_df['test_pos_rate'].values)):
-        assert r_b_dca_result_df['tp_rate'].values[i] == p_b_dca_result_df['tp_rate'].values[i]
-
-    # true positive rate
-    for i in range(0, len(r_b_dca_result_df['tp_rate'].values)):
-        assert r_b_dca_result_df['tp_rate'].values[i] == p_b_dca_result_df['tp_rate'].values[i]
-
-    # false positive rate
-    for i in range(0, len(r_b_dca_result_df['fp_rate'].values)):
-        assert r_b_dca_result_df['fp_rate'].values[i] == p_b_dca_result_df['fp_rate'].values[i]
-
-    # net benefit
-    for i in range(0, len(r_b_dca_result_df['net_benefit'].values)):
-        assert r_b_dca_result_df['net_benefit'].values[i] == p_b_dca_result_df['net_benefit'].values[i]
-
-
+# def test_b_dca_prev():
+#
+#     df_binary = load_binary_df()
+#     r_b_dca_result_df = load_r_bin_dca_result_2()
+#
+#     p_b_dca_result_df = \
+#         binary_dca(
+#             data=df_binary,
+#             outcome='cancer',
+#             predictors=['age'],
+#             probabilities=[True],
+#             prevalence=[0.5]
+#         )
+#
+#     del r_b_dca_result_df['label']
+#
+#     # Rounding to 13 digits to smooth out minute differences
+#     # between R and Python calcs
+#     r_b_dca_result_df = r_b_dca_result_df.round(13)
+#     p_b_dca_result_df = p_b_dca_result_df.round(13)
+#
+#     p_b_dca_result_df = p_b_dca_result_df.rename(columns={'fpr': 'fp_rate',
+#                                                           'tpr': 'tp_rate'})
+#
+#     r_b_dca_result_df = r_b_dca_result_df.sort_values(by=['variable', 'threshold'])
+#     p_b_dca_result_df = p_b_dca_result_df.sort_values(by=['variable', 'threshold'])
+#
+#     p_b_dca_result_df = p_b_dca_result_df[r_b_dca_result_df.columns]
+#
+#     # Compare values between r & p binary DCA result
+#
+#     # test_pos_rate
+#     for i in range(0, len(r_b_dca_result_df['test_pos_rate'].values)):
+#         assert r_b_dca_result_df['tp_rate'].values[i] == p_b_dca_result_df['tp_rate'].values[i]
+#
+#     # true positive rate
+#     for i in range(0, len(r_b_dca_result_df['tp_rate'].values)):
+#         assert r_b_dca_result_df['tp_rate'].values[i] == p_b_dca_result_df['tp_rate'].values[i]
+#
+#     # false positive rate
+#     for i in range(0, len(r_b_dca_result_df['fp_rate'].values)):
+#         assert r_b_dca_result_df['fp_rate'].values[i] == p_b_dca_result_df['fp_rate'].values[i]
+#
+#     # net benefit
+#     for i in range(0, len(r_b_dca_result_df['net_benefit'].values)):
+#         assert r_b_dca_result_df['net_benefit'].values[i] == p_b_dca_result_df['net_benefit'].values[i]
 
 
-
+# def test_load_newdata():
+#     asdf = pd.read_csv('https://raw.githubusercontent.com/ddsjoberg/dca-tutorial/main/data/df_cancer_dx.csv')
+#     print(asdf)
+#
+#     return

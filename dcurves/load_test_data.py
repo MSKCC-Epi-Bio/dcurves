c4375ce4848362140cf5ab83504fe9a4430e43fd
shaunporwal <shaun.porwal@gmail.com>
2023-01-03 23:46:07 -0500

    Test Simple Survival Case

--- dcurves/load_test_data.py
+++ dcurves/load_test_data.py
@@ -6,59 +6,38 @@ from os import path
 # root_test_dir = path.dirname(path.realpath(__file__))
 # resources_dir = path.join(root_test_dir, 'data')
 
-# path.join(resources_dir, 'df_surv.csv')
-# path.join(resources_dir, 'df_case_control.csv')
-
 # simdata_dir = path.join(resources_dir, 'sim_data')
 # r_results_dir = path.join(resources_dir, 'r_results')
 
 # Note: after speaking with Dan, can host data online on dropbox/s3/googledrive and pull using python script
 
-# Do try/except on pulling data if it's not in expected location
-
+# Load Simulation Data
 def load_binary_df():
-    """Return a dataframe containing a binary simulation dataset
-
-    Parameters
-    ----------
-    """
-
-    # This is a stream-like object. If you want the actual info, call
-    # stream.read()
-
-    # with pkg_resources.resource_stream(__name__, 'data/df_binary.csv') as stream:
-    #     # stream path:
-    #     # '/Users/ShaunPorwal/Documents/GitHub/python_packages/dcurves/dcurves/data/df_binary.csv'
-    #
-    #     data = pd.read_csv(stream, encoding='latin-
-
-    # data_path = path.join(resources_dir, 'df_binary.csv')
-    # data = pd.read_csv(data_path)
+    '''
+    Load Simulation Data For Binary Endpoints DCA
+    :return pd.DataFrame that contains binary data
+    '''
 
     stream = pkg_resources.resource_stream(__name__, 'data/df_binary.csv')
     return pd.read_csv(stream, encoding='latin-1')
 
 def load_survival_df():
-    """Return a dataframe containing a simulation survival data
     """
-    # This is a stream-like object. If you want the actual info, call
-    # stream.read()
-    # with pkg_resources.resource_stream(__name__, 'data/df_surv.csv') as stream:
-    #     data = pd.read_csv(stream, encoding='latin-1')
+    Load Simulation Data For Survival Endpoints DCA
+    :return pd.DataFrame that contains survival data
+    """
     stream = pkg_resources.resource_stream(__name__, 'data/df_surv.csv')
     return pd.read_csv(stream, encoding='latin-1')
 
 def load_case_control_df():
-    """Return a dataframe containing a case-control dataset
     """
-    # This is a stream-like object. If you want the actual info, call
-    # stream.read()
-
-    # with pkg_resources.resource_stream(__name__, 'data/df_case_control.csv') as stream:
-    #     data = pd.read_csv(stream, encoding='latin-1')
-
+    Load Simulation Data For Binary Endpoints DCA With User-Specified Outcome Prevalence (Case-Control)
+    :return pd.DataFrame that contains binary data
+    """
     stream = pkg_resources.resource_stream(__name__, 'data/df_case_control.csv')
     return pd.read_csv(stream, encoding='latin-1')
+
+# Load Benchmarking Data For QC On Local Functions
 
 def load_tutorial_bin_interventions_df():
     stream = pkg_resources.resource_stream(__name__, 'data/dca_tut_bin_int_df.csv')
@@ -74,9 +53,9 @@ def load_tutorial_coxph_pr_failure18_vals():
 def load_test_surv_risk_test_df():
     """
      These are risk scores calculated for marker convert_to_risk in survival case:
-     outcome = 'cancer'
-     model = 'marker'
-     data = load_survival_df() (from above)
+     outcome='cancer'
+     models=['marker']
+     data=load_survival_df() (from above)
      time=2,
      time_to_outcome_col='ttcancer'
     """
@@ -89,4 +68,17 @@ def load_tutorial_r_stdca_coxph_df():
 
 def load_tutorial_r_stdca_coxph_pr_failure18_test_consequences():
     stream = pkg_resources.resource_stream(__name__, 'data/dca_tut_r_stdca_coxph_pr_failure18_test_consequences.csv')
+    return pd.read_csv(stream, encoding='latin-1')
+
+def load_r_simple_surv_dca_result_df():
+    """
+    Dataframe containing results from r survival dca case for simple testing
+    outcome='cancer'
+    models=['famhistory']
+    thresholds=np.arange(0, 1.0, 0.01)
+    time_to_outcome_col='ttcancer'
+    time=1.5
+    :return:
+    """
+    stream = pkg_resources.resource_stream(__name__, 'data/r_simple_surv_dca_result_df.csv')
     return pd.read_csv(stream, encoding='latin-1')

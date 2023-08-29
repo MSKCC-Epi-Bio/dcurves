5288a490a74066961b37fa27cb58908e2086e782
PorwalS <PorwalS@mskcc.org>
2023-08-29 18:06:08 -0400

    Get tests to 100% coverage

--- tests/test_plot_smoothing.py
+++ tests/test_plot_smoothing.py
@@ -12,41 +12,79 @@ import lifelines
 
 def test_plot_smoothing():
 
-    pass
-    # from typing import List
-    # import lifelines
-    # from statsmodels.nonparametric.smoothers_lowess import lowess
+    from typing import List
+    import lifelines
 
-    # df_time_to_cancer_dx = \
-    #     pd.read_csv(
-    #         "https://raw.githubusercontent.com/ddsjoberg/dca-tutorial/main/data/df_time_to_cancer_dx.csv"
-    #     )
+    import statsmodels.api as sm
+    import numpy as np
+    import matplotlib.pyplot as plt
 
-    # cph = lifelines.CoxPHFitter()
-    # cph.fit(df=df_time_to_cancer_dx,
-    #         duration_col='ttcancer',
-    #         event_col='cancer',
-    #         formula='age + famhistory + marker')
+    import pandas
+    pandas.set_option('expand_frame_repr', False)
 
-    # cph_pred_vals = \
-    #     cph.predict_survival_function(
-    #         df_time_to_cancer_dx[['age',
-    #                               'famhistory',
-    #                               'marker']],
-    #         times=[1.5])
+    df_time_to_cancer_dx = \
+        pd.read_csv(
+            "https://raw.githubusercontent.com/ddsjoberg/dca-tutorial/main/data/df_time_to_cancer_dx.csv"
+        )
 
-    # df_time_to_cancer_dx['pr_failure18'] = [1 - val for val in cph_pred_vals.iloc[0, :]]
+    cph = lifelines.CoxPHFitter()
+    cph.fit(df=df_time_to_cancer_dx,
+            duration_col='ttcancer',
+            event_col='cancer',
+            formula='age + famhistory + marker')
 
-    # stdca_coxph_results = \
-    #     dca(
-    #         data=df_time_to_cancer_dx,
-    #         outcome='cancer',
-    #         modelnames=['pr_failure18'],
-    #         thresholds=[i/100 for i in range(0, 51)],
-    #         time=1.5,
-    #         time_to_outcome_col='ttcancer'
-    #     )
+    cph_pred_vals = \
+        cph.predict_survival_function(
+            df_time_to_cancer_dx[['age',
+                                  'famhistory',
+                                  'marker']],
+            times=[1.5])
 
+    df_time_to_cancer_dx['pr_failure18'] = [1 - val for val in cph_pred_vals.iloc[0, :]]
+
+    stdca_coxph_results = \
+        dca(
+            data=df_time_to_cancer_dx,
+            outcome='cancer',
+            modelnames=['pr_failure18'],
+            thresholds=[i/100 for i in range(0, 51)],
+            time=1.5,
+            time_to_outcome_col='ttcancer'
+        )
+
+    # plot_graphs(
+    #     plot_df=stdca_coxph_results,
+    #     graph_type='net_benefit'
+    # )
+
+    # print('\n')
+    # print(stdca_coxph_results)
+
+    # lowess = sm.nonparametric.lowess(stdca_coxph_results['net_benefit'].values, stdca_coxph_results['threshold'].values, frac=0.1)
+    #
+    # # print(lowess)
+    # stdca_coxph_results['smoothed_nb'] = lowess[:,1].copy()
+    # # print('\n')
+    # # print(stdca_coxph_results)
+    #
+    # import matplotlib.pyplot as plt
+    #
+    # # Scatter plot for the regular 'net_benefit'
+    # plt.scatter(stdca_coxph_results['threshold'], stdca_coxph_results['net_benefit'], label='net_benefit', alpha=0.5)
+    #
+    # # Line plot for the smoothed 'net_benefit'
+    # plt.plot(stdca_coxph_results['threshold'], stdca_coxph_results['smoothed_nb'], label='smoothed_net_benefit',
+    #          color='red')
+    #
+    # # Add labels and legend
+    # plt.xlabel('Threshold')
+    # plt.ylabel('Net Benefit')
+    # plt.title('Net Benefit vs Threshold')
+    # plt.legend()
+    #
+    # plt.show()
+
+    # sm.nonparametric.lowess(y, x, frac=0.3)
 
     # def polynomial_smooth(x: List[float], y: List[float], degree: int = 3, alpha: float = None) -> List[float]:
     #     if alpha is not None:
@@ -54,9 +92,7 @@ def test_plot_smoothing():
     #     else:
     #         y = lowess(y, x, return_sorted=False, frac=1. / degree)
     #     return y
-
-
-
+    #
     # stdca_coxph_results['smoothed_nb'] = polynomial_smooth(x=stdca_coxph_results['threshold'],
     #                                                        y=stdca_coxph_results['net_benefit'],
     #                                                        degree=4,
--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,73 +1,106 @@
-# Load Data
-from .load_test_data import load_binary_df
-
-# Load DCA function(s)
-from dcurves.dca import dca
-from dcurves.plot_graphs import _plot_net_benefit, _plot_net_intervention_avoided, plot_graphs
-
-# Load Libraries Used in Testing
 import pandas as pd
 import numpy as np
+import random
+import re
+import pytest
+from unittest.mock import patch
+
+from .load_test_data import load_binary_df
+from dcurves.dca import dca
+from dcurves.plot_graphs import (
+    plot_graphs,
+    _plot_net_benefit,
+    _plot_net_intervention_avoided,
+    _get_colors,
+)
+
+# Constants
+SAMPLE_DATA_DF = pd.DataFrame({
+    "model": ["Model1", "Model2"],
+    "threshold": [0.2, 0.5],
+    "net_benefit": [0.7, 0.8],
+    "net_intervention_avoided": [0.3, 0.4],
+})
+
+DEFAULT_COLOR_NAMES = ["red", "blue"]
+
+
+def get_dca_results(data, thresholds=None):
+    return dca(
+        data=data,
+        outcome='cancer',
+        modelnames=['famhistory'],
+        thresholds=thresholds or np.arange(0, 0.5, 0.01)
+    )
+
 
 def test_2_case1_plot_net_benefit():
-
     df_cancer_dx = pd.read_csv('https://raw.githubusercontent.com/ddsjoberg/dca-tutorial/main/data/df_cancer_dx.csv')
-
-    dca_famhistory_df = \
-        dca(
-            data=df_cancer_dx,
-            outcome='cancer',
-            modelnames=['famhistory'],
-            thresholds=[i/100 for i in range(0, 46)]
-        )
-
-    # plot_graphs(
-    #     plot_df=dca_famhistory_df,
-    #     graph_type='net_benefit',
-    #     y_limits=[-0.05, 0.15],
-    #     color_names=['blue', 'red', 'green']
-    # )
+    dca_famhistory_df = get_dca_results(df_cancer_dx, [i/100 for i in range(0, 46)])
 
 
 def test_case1_plot_net_benefit():
-
     data = load_binary_df()
-    dca_results = \
-        dca(
-            data=data,
-            outcome='cancer',
-            modelnames=['famhistory'],
-            thresholds=np.arange(0, 0.5, 0.01)
-        )
+    get_dca_results(data)
 
-    plot_df = dca_results
-
-    # _plot_net_benefit(
-    #     plot_df=plot_df,
-    #     color_names=['blue', 'red', 'green'],
-    #     y_limits=[-0.05, 0.3]
-    # )
 
 def test_case1_plot_net_intervention_avoided():
-
     data = load_binary_df()
-    y_limits = [-0.05, 1]
+    get_dca_results(data)
 
-    dca_results = \
-        dca(
-            data=data,
-            outcome='cancer',
-            modelnames=['famhistory']
-        )
 
-    plot_df = dca_results
+def test_get_colors():
+    # Set a seed for repeatability
+    random.seed(42)
 
-    # _plot_net_intervention_avoided(
-    #     plot_df=plot_df,
-    #     y_limits=y_limits,
-    #     color_names=['blue', 'red', 'green']
-    # )
+    num_colors = 5
+    colors = _get_colors(num_colors=num_colors)
+    
+    # Check that the number of colors returned is correct
+    assert len(colors) == num_colors, f"Expected {num_colors} colors, got {len(colors)} colors."
 
-def test_plot_rough_dca():
+    # Check that each color is a valid color code
+    for color in colors:
+        assert re.match(r'^#[0-9A-Fa-f]{6}$', color), f"Invalid color code: {color}"
 
-    pass
+    # Check for repeatability using a seed
+    random.seed(42)
+    assert colors == _get_colors(num_colors=num_colors), "Expected the same colors with the same seed."
+
+
+@pytest.mark.parametrize(
+    "func, args",
+    [
+        (_plot_net_benefit, {"y_limits": (-0.05, 1)}),
+        (_plot_net_intervention_avoided, {}),
+    ]
+)
+def test_plot_functions(func, args, mocker):
+    mocker.patch("matplotlib.pyplot.plot", autospec=True)
+    mocker.patch("matplotlib.pyplot.show", autospec=True)
+    func(SAMPLE_DATA_DF, color_names=DEFAULT_COLOR_NAMES, **args)
+
+
+def test_plot_graphs():
+    # The plot_graphs functions can be tested together using pytest's parametrize feature
+    with patch("dcurves.plot_graphs._plot_net_benefit") as mock_plot_net_benefit:
+        plot_graphs(plot_df=SAMPLE_DATA_DF, graph_type="net_benefit", color_names=DEFAULT_COLOR_NAMES)
+        mock_plot_net_benefit.assert_called_once_with(plot_df=SAMPLE_DATA_DF, y_limits=(-0.05, 1), color_names=DEFAULT_COLOR_NAMES)
+
+    with patch("dcurves.plot_graphs._plot_net_intervention_avoided") as mock_plot_net_intervention_avoided:
+        plot_graphs(plot_df=SAMPLE_DATA_DF, graph_type="net_intervention_avoided", color_names=DEFAULT_COLOR_NAMES)
+        mock_plot_net_intervention_avoided.assert_called_once_with(plot_df=SAMPLE_DATA_DF, y_limits=(-0.05, 1), color_names=DEFAULT_COLOR_NAMES)
+
+
+def test_plot_graphs_exceptions():
+    with pytest.raises(ValueError):
+        plot_graphs(plot_df=SAMPLE_DATA_DF, graph_type="invalid_type")
+
+    with pytest.raises(ValueError):
+        plot_graphs(plot_df=SAMPLE_DATA_DF, graph_type="net_benefit", color_names=["red"])
+
+    with patch("dcurves.plot_graphs._get_colors", return_value=DEFAULT_COLOR_NAMES) as mock_get_colors:
+        with patch("dcurves.plot_graphs._plot_net_benefit") as mock_plot_net_benefit:
+            plot_graphs(plot_df=SAMPLE_DATA_DF, graph_type="net_benefit")
+            mock_get_colors.assert_called_once_with(num_colors=2)
+            mock_plot_net_benefit.assert_called_once_with(plot_df=SAMPLE_DATA_DF, y_limits=(-0.05, 1), color_names=DEFAULT_COLOR_NAMES)

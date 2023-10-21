41d1be75c502782f3c2a07e19847b6072720192e
PorwalS <PorwalS@mskcc.org>
2023-10-20 20:37:08 -0400

    Refactor Data Loading and Add Descriptions
    1. Added a unified load_data function to load data files based on provided filename.
    2. Incorporated a mapping of filenames to their corresponding descriptions within the load_data function.
    3. emoved the load_binary_df function as its functionality is now covered by the unified function.
    4. Checked if the provided filename is in the mapping and raised a ValueError for unknown filenames.
    5. Modified the way files are loaded to handle different encodings (latin-1).
    6. Added/updated descriptions for various data files, including external links and detailed descriptions for specific datasets.

--- tests/load_test_data.py
+++ tests/load_test_data.py
@@ -3,12 +3,44 @@ import pathlib
 
 import pandas as pd
 
-# Note: after speaking with Dan, can host data online on dropbox/s3/googledrive and pull using python script
+def load_data(filename):
+    """
+    Unified function to load a data file based on the provided filename.
+    :param filename: Name of the file to be loaded.
+    :return: pd.DataFrame with the loaded data.
+    """
 
-# def load_binary_df():
-#     current_dir = pathlib.Path(__file__).parent
-#     file_path = current_dir / 'benchmark_data/df_binary.csv'
-#     return pd.read_csv(file_path, encoding='latin-1')
+    # Define a mapping of filenames to their corresponding descriptions
+    descriptions = {
+        'df_binary.csv': 'Load Simulation Data For Binary Endpoints DCA',
+        'df_surv.csv': 'Load Simulation Data For Survival Endpoints DCA',
+        'df_case_control.csv': 'Load Simulation Data For Binary Endpoints DCA With User-Specified Outcome Prevalence (Case-Control)',
+        'shishir_simdata.csv': 'Load simulation survival data from Shishir Rao, Oxford Doctoral Student, and check that my DCA functions work correctly',
+        'r_case1_results.csv': 'Binary, cancer ~ famhistory',
+        'r_case2_results.csv': 'Survival, Cancer ~ Cancerpredmarker',
+        'r_case3_results.csv': 'Binary, Cancer ~ marker',
+        'dca_tut_bin_int_marker_risks.csv': '',
+        'dca_tut_coxph_pr_failure18_vals.csv': '',
+        'surv_risk_test_df.csv': 'These are risk scores calculated for marker convert_to_risk in survival case',
+        'dca_tut_r_stdca_coxph_df.csv': '',
+        'dca_tut_r_stdca_coxph_pr_failure18_test_consequences.csv': '',
+        'r_simple_surv_dca_result_df.csv': "Dataframe containing results from r survival dca case for simple testing",
+        'r_simple_surv_tpfp_calc_df.csv': "Dataframe containing columns used to calculate tp/fp rate from r survival dca case for simple testing",
+        'r_simple_binary_dca_result_df.csv': '',
+        'r_dca_famhistory.csv': '',
+        'df_cancer_dx.csv': 'Load data that can also be gotten from this link: "https://raw.githubusercontent.com/ddsjoberg/dca-tutorial/main/data/df_time_to_cancer_dx.csv"',
+        'df_cancer_dx2.csv': 'Load data that is same as df_cancer_dx but with predictions from model as in dca-tutorial'
+    }
+
+    # Check if the filename is in the mapping
+    if filename not in descriptions:
+        raise ValueError(f"Unknown filename: {filename}")
+
+    # Load the file
+    file_path = os.path.join(os.path.dirname(__file__), f'benchmark_data/{filename}')
+    with open(file_path, 'r') as f:
+        return pd.read_csv(f, encoding='latin-1')
+
 
 def load_data_file(filename):
     file_path = os.path.join(os.path.dirname(__file__), f'benchmark_data/{filename}')

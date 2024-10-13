0247409fca3a4367ace3040b391c10e857ef6904
PorwalS <PorwalS@mskcc.org>
2024-10-13 18:45:38 -0400

    Test tox, update tox.ini

--- tests/test_competing_risks.py
+++ tests/test_competing_risks.py
@@ -1,12 +1,53 @@
-def get_estimated_cif(durations, events, event_of_interest=1, time_of_interest=180.0):
+
+from dcurves.risks import _calc_surv_risks
+from .load_test_data import load_data
+import numpy as np
+import pandas as pd  # Assuming the output is a DataFrame
+import pytest
+
+from statsmodels.duration.hazard_regression import PHReg
+from statsmodels.duration.api import CumIncidence
+
+def test_regular_surv_case_new_code():
+    
+    
+    data = load_data("df_surv.csv")
+    
+    
+    
     pass
 
-    # import lifelines
-    # """Get Competing Risk Estimate """
-    # ajf = lifelines.AalenJohansenFitter(calculate_variance=True)
 
-    # ajf.fit(durations, events, event_of_interest=event_of_interest)
+def test_competing_risks():
+    
+    data = load_data("df_surv.csv")
+    
+    # dca_results = dca(
+    #     data=data,
+    #     outcome="cancer",
+    #     modelnames=["famhistory", "marker", "cancerpredmarker"],
+    #     models_to_prob=["marker"],
+    #     thresholds=[i / 100 for i in range(0, 46)],
+    #     time_to_outcome_col="ttcancer",
+    #     time=1
+    # )
+    
+    model = 'marker' # to convert to prob
+    outcome = 'cancer'
+    time = 1
+    time_to_outcome_col = "ttcancer"
 
-    # res = ajf.cumulative_density_.loc[time_of_interest].values[0]
 
-    # return res
+    cph_df = data[[time_to_outcome_col, outcome, model]].copy()
+    cph = lifelines.CoxPHFitter()
+    cph.fit(cph_df, time_to_outcome_col, outcome)
+    cph_df = cph_df.assign(**{time_to_outcome_col: time}).copy()
+    predicted_vals = cph.predict_survival_function(cph_df, times=time).values[0]
+    
+    
+    data = data.assign(**{model: predicted_vals}).copy()
+
+    data = data.assign(**{"all": 1}).copy()
+    data = data.assign(**{"none": 0}).copy()
+    
+    print(data)

d0095bf71b029bf2fe3595336ce4d17ca24d0626
Shaun Porwal <shaun.porwal@gmail.com>
2025-02-20 00:47:29 -0500

    Update toml, use poetry dynamic versioning

--- dcurves/__init__.py
+++ dcurves/__init__.py
@@ -10,11 +10,11 @@ Modules exported by this package:
 
 """
 
-__version__ = "1.1.1"
-
 import os
 from dcurves import load_test_data
 from dcurves.dca import dca
 from dcurves.plot_graphs import plot_graphs
+
+__all__ = ["load_test_data", "dca", "plot_graphs"]
 
 data = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
